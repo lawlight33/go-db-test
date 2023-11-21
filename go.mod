f730586255e99fb20787f40d8cacb2702793c0b9
Mark <promark33@gmail.com>
2023-11-21 15:42:55 +0100

    Parallel inserts

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-db-test
 
 go 1.21.4
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/google/uuid v1.4.0
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -3,30 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
+	"sync"
 
-	_ "github.com/lib/pq" // Import the PostgreSQL driver
+	"github.com/google/uuid"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbHost      = "localhost"
+	dbPort      = 5432
+	dbUser      = "postgres"
+	dbPassword  = "postgres"
+	dbName      = "postgres"
+	threadCount = 50
 )
 
 func main() {
-	fmt.Println("List Postgres roles ...")
-
-	dbHost := "localhost"
-	dbPort := 5432
-	dbUser := "postgres"
-	dbPassword := "postgres"
-	dbName := "postgres"
+	fmt.Println("Running ...")
 
 	connectionString := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPassword, dbName,
 	)
 
+	// Connect to the Postgres database
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	// Make select operation
+	selectExample(db)
+
+	// Make insert operation
+	parallelInsertExample(db, threadCount)
+
+	fmt.Println("Done.")
+}
+
+func selectExample(db *sql.DB) {
+	fmt.Println("Listing Postgres roles:")
 	rows, err := db.Query("select rolname from pg_roles")
 	if err != nil {
 		panic(err)
@@ -41,4 +60,32 @@ func main() {
 		}
 		fmt.Println("> " + name)
 	}
+}
+
+func parallelInsertExample(db *sql.DB, threads int) {
+	query := `insert into loadtest values ($1, $2, $3);`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(threads)
+	for i := 0; i < threads; i++ {
+		var index = i
+		go func() {
+			defer wg.Done()
+			fmt.Println("Insert from thread #" + strconv.Itoa(index))
+			id1 := uuid.NewString()
+			id2 := uuid.NewString()
+			id3 := uuid.NewString()
+			parameters := []interface{}{id1, id2, id3}
+			_, err := stmt.Exec(parameters...)
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	wg.Wait()
 }
